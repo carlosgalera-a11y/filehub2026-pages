44ecf5e091f691d8b650e07cb301ef617c843908
carlosg <carlosgalera2roman@gmail.com>
2026-07-15 16:11:45 +0200

    🪞 Mirror v221 EGS TeK (manual)

--- assets/index-CLRR9vQU.js
+++ assets/index-CLRR9vQU.js
@@ -1,4 +1,4 @@
-import{a as is}from"./index-D_DGDLNf.js";/*! *****************************************************************************
+import{a as is}from"./index-gcYtKrfT.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
